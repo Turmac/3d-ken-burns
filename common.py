ecb858c98edc3df2524f39491fc5516a6eb57947
Qinbo Li <sysulqb@gmail.com>
2020-05-05 14:29:34 -0500

    Update common.py

--- common.py
+++ common.py
@@ -202,8 +202,8 @@ def process_kenburns(objSettings):
 			npyOutputs.append(npyOutput)
 	# end
 	
-	numpy.save('depth.npy', objectCommon['tensorRawDisparity'].detach().cpu().numpy())
-	numpy.save('inp_depth.npy', objectCommon['tensorInpaDepth'].detach().cpu().numpy())
+	numpy.save('depth.npy', objectCommon['tenRawDisparity'].detach().cpu().numpy())
+	numpy.save('inp_depth.npy', objectCommon['tenInpaDepth'].detach().cpu().numpy())
 
 	return npyOutputs
 # end
